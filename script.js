82a9e21e9495ade863ef731cd06e53209c642bdd
sahilkumar013 <117899274+sahilkumar013@users.noreply.github.com>
2023-03-18 14:05:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,10 +10,16 @@ const lookup = {
   'Y': 'L','Z': 'M', '?': '?', ',': ','
 };
 
+
 function rot13(encodedStr){
    let decodedArr = []; // Your Result goes here
   // Only change code below this line
-
+	const string = encodedStr;
+	const arr = [];
+ for(let i = 0;i<string.length;i++){
+	string.charAt(i) = string.charAt(i+13);
+	 arr.push(string[i]);
+ }
   return ;//return decodedArr
 }
 
